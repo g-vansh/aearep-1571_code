97878cb1374c40d2480339d35cd1ceecb99d9856
Lars Vilhuber <lars.vilhuber@cornell.edu>
2021-07-30 14:45:58 -0400

    Updates

--- Code/run_script.py
+++ Code/run_script.py
@@ -1,32 +1,48 @@
 # Import Packages
 import glob, os
+import sys
 import requests
 import json
 import ntpath
 
 # Change Directory To The Root Directory, please ensure that there is no slash symbol in the end
-root_dir = "C:/Users/vg224_RS/sample_folder"
+#root_dir = "C:/Users/vg224_RS/sample_folder"
+root_dir = sys.argv[1]
 os.chdir(root_dir)
-search_path = root_dir + "/**/"
+search_path = root_dir + "/"
 
 # Extension To Be Searched
-ext = "*.zip"
+#ext = "*.py"
+ext = sys.argv[2]
 search_pattern = search_path + ext
 
+print("Search pattern:" + search_pattern)
+
 # Change Access Token
-ACCESS_TOKEN = "CHANGE_ME"
+#ACCESS_TOKEN = "CHANGE_ME"
+ACCESS_TOKEN = sys.argv[3]
+DEPOSITION_ID = "4317553"
+# Base URL: could be the sandbox!
+BASE_URL = "https://zenodo.org/api/deposit/depositions"
 
 # Would you like to search the subdirectories in the directory as well? 
 # "True" for Yes, "False" for No. 
 # "True" by default.
-subdir = True 
+subdir = False
 
 # Configuring Script For Zenodo Upload
 params = {'access_token': ACCESS_TOKEN}
 headers = {"Content-Type": "application/json"}
-r = requests.post('https://zenodo.org/api/deposit/depositions',params = params, headers = headers, json = {})
+# If creating a new deposit, then the post will create that new deposit
+#r = requests.post(BASE_URL,params = params, headers = headers, json = {})
+# However, if we already have a deposit, we want to get the information
+r = requests.get(BASE_URL + '/' + DEPOSITION_ID,params = params, headers = headers, json = {})
 bucket_url = r.json()['links']['bucket']
-
+print("Uploading to start: ")
+print(" Deposit          : " + DEPOSITION_ID)
+print(" (check at " + BASE_URL + '/' + DEPOSITION_ID + ")")
+print(" Bucket           : " + bucket_url )
+input("Press Enter to continue...")
 # Parsing through each file and uploading it
 for file in glob.glob(search_pattern, recursive = subdir):
     filename = ntpath.basename(file)
